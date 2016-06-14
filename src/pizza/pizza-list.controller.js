4a6ac9a1b2dfd5593448ab6d4a98787de4edcbe1
DTA <DTA>
2016-06-14 10:55:00 +0200

    tp prof pour suivre

--- src/pizza/pizza-list.controller.js
+++ src/pizza/pizza-list.controller.js
@@ -1,17 +1,35 @@
-import {Pizza, map} from './Pizza.js'
-import {ListPizza} from './ListPizza.js'
+import { Pizza } from './pizza'
 
 export class PizzaListController {
-    constructor() {
-        this.pizzas = new ListPizza()
-        this.pizzas.addPizza(new Pizza('reine'))
-        this.pizzas.addPizza(new Pizza('marguarita'))
-    }
-    addPizzaClick(pizzaName){
-        pizzas.addPizza(new Pizza(pizzaName))
-    }
+  constructor ($timeout) {
+    this.$timeout = $timeout
 
-    getPizzas(){
-        
-    }
+    this.pizzas = [
+      new Pizza({ name: 'un', status: 'not cooked', toppings: ['eggs', 'mushrooms'] }),
+      new Pizza({ name: 'deux', status: 'not cooked', toppings: [] }),
+      new Pizza({ name: 'trois', status: 'not cooked', toppings: ['eggs', 'eggs', 'mushrooms'] }),
+      new Pizza({ name: 'quatre', status: 'not cooked' }),
+      new Pizza({ name: 'cinq', status: 'not cooked' })
+    ]
+  }
+
+  addPizza () {
+    this.pizzas.push({
+      name: 'new pizza'
+    })
+  }
+
+  cookPizza (pizza) {
+    return this.$timeout(() => {
+      pizza.status = 1
+    }, 3000)
+  }
+
+  cookPizzas () {
+    const pizza = this.pizzas.find(p => p.status === 0)
+    if (!pizza) return
+    this.cookPizza(pizza)
+      .then(this.cookPizzas.bind(this))
+  }
+
 }
--- src/pizza/pizza.js
+++ src/pizza/pizza.js
@@ -1,103 +1,26 @@
-export var _map = {
-  'tomato_sauce': {'fr': 'sauce tomate', 'en': 'tomato sauce'},
-  'mozzarella': {'fr': 'mozzarelle', 'en': 'mozzarella'},
-  'mushrooms': {'fr': 'champignon', 'en': 'mushrooms'},
-  'ham': {'fr': 'jambon', 'en': 'ham'},
-  'eggs': {'fr': 'oeuf', 'en': 'eggs'},
-  'artichoke': {'fr': 'articho', 'en': 'artichoke'},
-  'green_olives': {'fr': 'olives verte', 'en': 'green olives'},
-  'onion': {'fr': 'oignon', 'en': 'onion'},
-  'sweet_corn': {'fr': 'mais', 'en': 'sweet corn'},
-  'green_peppers': {'fr': 'poivre vert', 'en': 'green peppers'},
-  'black_olives': {'fr': 'olive noir', 'en': 'black olives'},
-  'peas': {'fr': 'pois', 'en': 'peas'},
-  'salami': {'fr': 'salami', 'en': 'salami'}
-}
-
 export class Pizza {
-  constructor (name, toppings, isCook) {
-    this.name = name || 'default_name'
-    this.toppings = toppings || []
-    this.isCook = isCook || false
+  constructor ({name, toppings, status}) {
+    this.name = name
+    this.toppings = toppings
+    this.status = status
   }
 
-  monNom () {
-    return this.name
-  }
+  toppings2string (pizza) {
+    if (!this.toppings) return ''
+    return this.toppings
 
-  setNom (name) {
-    this.name = name || 'default_name'
-    return this
-  }
+      // uniqs
+      .reduce((acc, topping) => {
+        if (acc.indexOf(topping) === -1) acc.push(topping)
+        return acc
+      }, [])
 
-  addTopping (toppings) {
-    const filtre = function (v) {
-      return v === toppings
-    }
-
-    if (Object.keys(_map).filter(filtre).length !== 0 && this.toppings.filter(filtre).length < 2) {
-      this.toppings.push(toppings)
-    }
-    return this
-  }
-
-  deleteTopping (toppings) {
-    const filtre = function (v) {
-      return v !== toppings
-    }
-    this.toppings = this.toppings.filter(filtre)
-    return this
-  }
- toppingsToString(){
-   let retour =""
-    this.toppings.forEach(function (element) {
-      retour = retour + element
-    })
-   return retour
- }
-  toString (lang = 'en') {
-    var chaine = ''
-    const uniqueArray = this.toppings.reduce(function (accu, item, index, array) {
-      if (accu.indexOf(item) === -1) {
-        accu.push(item)
-      }
-      return accu
-    }, [])
-    const self = this
-    chaine += '<td>'
-    uniqueArray.forEach(function (item, index, array) {
-      var nb = self.toppings.reduce(function (accu, item1) {
-        if (item1 === item) {
-          return accu + 1
-        }
-        return accu
-      }, 0)
-      chaine += self.translate(item, lang) + ' (' + nb + ')'
-      if (index < array.length - 1) chaine += ','
-    })
-    return chaine + '</td>'
-  }
-
-  translate (topping, lang = 'en') {
-    return _map[topping][lang] || topping
-  }
-
-  isToppingPresent (topping) {
-    if (this.toppings.indexOf(topping) === -1) {
-      return false
-    }
-    return true
-  }
-
-  cook (time = 1000) {
-    console.log('pizza lancer')
-    return new Promise((resolve, reject) => {
-      if (this.isCook) return reject('pizza déjà cuite')
-
-      setTimeout(() => {
-        this.isCook = true
-        resolve()
-      }, time)
-    })
+      // topping (translated (nb))
+      .map(topping => {
+        const size = this.toppings.filter(item => item === topping).length
+        if (size > 1) return `${topping} x${size}`
+        return `${topping}`
+      })
+      .join(', ')
   }
 }
